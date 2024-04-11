93074e40e97e819a34cfdcf1c2775b00e83f4d20
Kholekile2 <5100146@mypillar7uwc.ac.za>
2024-04-11 15:07:23 +0200

    Add project files.

---
+++ FirstClassExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FirstClassExercise
+{
+    internal class Program
+    {
+      public static string ReverseString(string forwardText)
+        {
+            
+            string containerText = "";
+
+            for (int i = forwardText.Length - 1; i >= 0; i--)
+            {
+                containerText += forwardText[i];
+            }
+
+            return containerText;
+
+        }
+        static void Main(string[] args)
+        {
+            string reversedString = ReverseString("Text to reverse.");
+
+            Console.WriteLine("Output: " + reversedString);
+
+        }
+    }
+}
